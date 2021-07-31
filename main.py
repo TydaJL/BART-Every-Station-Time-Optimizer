137428c73ba7a69c7412c6cc5a935bb8563cc06b
TydaJL <tydalamb@gmail.com>
2021-07-30 22:15:47 -0700

    Support for AM/PM times, moving testing
    moving testing to test.py to reduce clutter in main.py
    
    Added support for AM/PM times. 24 hour times would work as well, but as BART's timetables run on 12 hour time, I added support for that.

--- main.py
+++ main.py
@@ -8,16 +8,16 @@ from datetime import timedelta
 lines = ["r", "b", "o", "g", "y"]
 
 #Initializing First 5 Train Times for All Lines
-r_S = [["5:00", "5:11", "5:26", "5:41", "5:56"], ["6:20", "6:30", "6:45", "7:00", "7:15"]] #Richmond > Millbrae
-r_N = [["6:23", "6:38", "6:53", "7:08", "7:23"], ["7:40", "7:55", "8:10", "8:25", "8:40"]] #Millbrae > Richmond
-b_W = [["5:06", "5:21", "5:36", "5:51", "6:06"], ["6:10", "6:25", "6:40", "6:55", "7:10"]] #Dublin/Pleasanton > Daly City
-b_E = [["5:12", "5:27", "5:42", "5:57", "6:12"], ["6:16", "6:31", "6:46", "7:01", "7:16"]] #Daly City > Dublin/Pleasanton
-o_N = [["4:59", "5:14", "5:29", "5:44", "5:59"], ["6:20", "6:35", "6:50", "7:05", "7:20"]] #Berryessa/North San Jose > Richmond
-o_S = [["5:03", "5:18", "5:33", "5:48", "6:03"], ["6:26", "6:40", "6:55", "7:10", "7:25"]] #Richmond > Berryessa/North San Jose
-y_W = [["4:47", "5:02", "5:17", "5:32", "5:47"], ["6:28", "6:43", "6:58", "7:13", "7:28"]] #Antioch > SFO
-y_E = [["5:11", "5:26", "5:41", "5:56", "6:11"], ["6:35", "6:50", "7:05", "7:20", "7:35"]] #SFO > Antioch
-g_W = [["4:40", "4:55", "5:10", "5:25", "5:40"], ["6:03", "6:18", "6:33", "6:48", "7:03"]] #Berryessa/North San Jose > Daly City
-g_E = [["6:05", "6:20", "6:35", "6:50", "7:05"], ["7:29", "7:44", "7:59", "8:14", "8:29"]] #Daly City > Berryessa/North San Jose
+r_S = [["5:00 AM", "5:11 AM", "5:26 AM", "5:41 AM", "5:56 AM"], ["6:20 AM", "6:30 AM", "6:45 AM", "7:00 AM", "7:15 AM"]] #Richmond > Millbrae
+r_N = [["6:23 AM", "6:38 AM", "6:53 AM", "7:08 AM", "7:23 AM"], ["7:40 AM", "7:55 AM", "8:10 AM", "8:25 AM", "8:40 AM"]] #Millbrae > Richmond
+b_W = [["5:06 AM", "5:21 AM", "5:36 AM", "5:51 AM", "6:06 AM"], ["6:10 AM", "6:25 AM", "6:40 AM", "6:55 AM", "7:10 AM"]] #Dublin/Pleasanton > Daly City
+b_E = [["5:12 AM", "5:27 AM", "5:42 AM", "5:57 AM", "6:12 AM"], ["6:16 AM", "6:31 AM", "6:46 AM", "7:01 AM", "7:16 AM"]] #Daly City > Dublin/Pleasanton
+o_N = [["4:59 AM", "5:14 AM", "5:29 AM", "5:44 AM", "5:59 AM"], ["6:20 AM", "6:35 AM", "6:50 AM", "7:05 AM", "7:20 AM"]] #Berryessa/North San Jose > Richmond
+o_S = [["5:03 AM", "5:18 AM", "5:33 AM", "5:48 AM", "6:03 AM"], ["6:26 AM", "6:40 AM", "6:55 AM", "7:10 AM", "7:25 AM"]] #Richmond > Berryessa/North San Jose
+y_W = [["4:47 AM", "5:02 AM", "5:17 AM", "5:32 AM", "5:47 AM", "6:00 AM", "6:17 AM", ], ["6:28 AM", "6:43 AM", "6:58 AM", "7:13 AM", "7:28 AM"]] #Antioch > SFO
+y_E = [["5:11 AM", "5:26 AM", "5:41 AM", "5:56 AM", "6:11 AM"], ["6:35 AM", "6:50 AM", "7:05 AM", "7:20 AM", "7:35 AM"]] #SFO > Antioch
+g_W = [["4:40 AM", "4:55 AM", "5:10 AM", "5:25 AM", "5:40 AM"], ["6:03 AM", "6:18 AM", "6:33 AM", "6:48 AM", "7:03 AM"]] #Berryessa/North San Jose > Daly City
+g_E = [["6:05 AM", "6:20 AM", "6:35 AM", "6:50 AM", "7:05 AM"], ["7:29 AM", "7:44 AM", "7:59 AM", "8:14 AM", "8:29 AM"]] #Daly City > Berryessa/North San Jose
 
 r_SConvertedMinutes = [[0 for numRow in range(len(r_S[0]))] for numCol in range(len(r_S))]
 r_NConvertedMinutes = [[0 for numRow in range(len(r_N[0]))] for numCol in range(len(r_N))]
@@ -59,8 +59,10 @@ x = 0
 def convertIntoMinutes(timestampArray, convertedIntoMinutesArray): #timestamp Array is r, b, o, g, y; convertedIntoMinutesArray is rConvertedMinutes, etc.
     for row in range(len(timestampArray)):
         for col in range(len(timestampArray[row])):
-            tDelta = timedelta(hours=int(timestampArray[row][col].split(':')[0]), minutes=int(timestampArray[row][col].split(':')[1]))
+            tDelta = timedelta(hours=int(timestampArray[row][col].split(':')[0]), minutes=int(timestampArray[row][col].split(':')[1].strip(" APM")))
             minutesAfterMidnight = int(tDelta.total_seconds() / 60)
+            if " PM" in timestampArray[row][col]:
+                minutesAfterMidnight += 720
             convertedIntoMinutesArray[row][col] = minutesAfterMidnight
 
 def convertAllIntoMinutes(allList):
@@ -77,10 +79,6 @@ def alltimeToComplete(allTimesList):
 
 
 def lowestTimeToComplete(timeList):
-    #lowestTime = timeList[0]
-    #for i in range(len(timeList)):
-    #    if(timeList[i]) < lowestTime:
-    #        lowestTime = timeList[i]
     return min(timeList)
 
 def lowestTimeAllLines():
@@ -94,28 +92,3 @@ def lowestTimeAllLines():
 
 def printAllLines():
     print(lineList)
-
-#Testing Cases
-
-#convertIntoMinutes(r_S, r_SConvertedMinutes)
-#convertIntoMinutes(b_W, b_WConvertedMinutes)
-
-convertAllIntoMinutes(lineList)
-timeToComplete(r_NConvertedMinutes, r_NTime)
-alltimeToComplete(completeTime)
-print(r_NConvertedMinutes)
-print(r_NTime)
-lowestTimeToComplete(r_NTime)
-print(completeTime)
-lowestTimeAllLines()
-
-#print(lineList)
-#print(convertedList)
-
-
-
-#for row in range(len(r)):
-#    for col in range(len(r[row])):
-#        rConvertedMinutes[row][col] = timedelta(hours = r[row][col], minutes ) j
-
-# print(rConvertedMinutes)
---
+++ test.py
@@ -0,0 +1,10 @@
+from main import *
+
+convertAllIntoMinutes(lineList)
+timeToComplete(r_NConvertedMinutes, r_NTime)
+alltimeToComplete(completeTime)
+print(r_NConvertedMinutes)
+print(r_NTime)
+lowestTimeToComplete(r_NTime)
+print(completeTime)
+lowestTimeAllLines()
